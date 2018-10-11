384bd071dc867f0ab2b2b604b8bd2ddc7bae1a79
Tomasz Gieszczyk <tomasz.gieszczyk@ah.nl>
2018-10-11 23:21:09 +0200

    test

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # always-delivery
+
+# First time run:
+<code>docker-compose up</code>
+
+
+# Run it seconds times:
+
+<code>docker-compose build --no-cache allways-deliver</code>
+
+<code>docker-compose up</code>
--- src/main/java/com/ahold/alwaysdelivery/config/MongoReactiveApplication.java
+++ src/main/java/com/ahold/alwaysdelivery/config/MongoReactiveApplication.java
@@ -1,21 +1,20 @@
 package com.ahold.alwaysdelivery.config;
 
 import com.mongodb.reactivestreams.client.MongoClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 
 public class MongoReactiveApplication extends AbstractReactiveMongoConfiguration {
 
     @Override
     protected String getDatabaseName() {
-        return "MyDb";
+        return "ALLDELIV";
     }
 
     @Bean
     public MongoClient reactiveMongoClient() {
-        return com.mongodb.reactivestreams.client.MongoClients.create((String)"mongodb://mongo-delivery:27017/ALLDELIV");
+        return com.mongodb.reactivestreams.client.MongoClients.create((String) "mongodb://mongo-delivery:27017/ALLDELIV");
     }
 }
