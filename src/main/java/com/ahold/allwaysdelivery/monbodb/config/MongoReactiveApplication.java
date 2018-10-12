68d6bc839296cc45c3407637dcd017afe5cd8959
Tomasz Gieszczyk <tomasz.gieszczyk@ah.nl>
2018-10-12 12:17:50 +0200

    build

--- src/main/java/com/ahold/allwaysdelivery/monbodb/config/MongoReactiveApplication.java
+++ src/main/java/com/ahold/allwaysdelivery/monbodb/config/MongoReactiveApplication.java
@@ -24,7 +24,7 @@ public class MongoReactiveApplication extends AbstractReactiveMongoConfiguration
 
     @Bean
     public MongoClient reactiveMongoClient() {
-        return com.mongodb.reactivestreams.client.MongoClients.create((String) "mongodb://mongo-delivery:27017/ALLDELIV");
+        return com.mongodb.reactivestreams.client.MongoClients.create((String) "mongodb://ah-allways-delivery-mongo:27017/ALLDELIV");
     }
 
     @Bean
